a58028e057cb260c6680e16a9b3ad790bf7064f4
Nathan-Edward SONGHO-MALONGA <SONGHO-MALONGA_Na@lprs.local>
2024-10-01 15:13:29 +0200

    debut tp

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module appli.todolist {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens appli.todolist to javafx.fxml;
+    exports appli.todolist;
+}
